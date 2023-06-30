1cf905d982e6e3f460f4a00c4bdb48c7eaec10a3
Shantanu Sutar <shantanusutar17@gmail.com>
2023-06-30 19:36:04 +0530

    displaying a list of friends
    also displaying who owes money to whom on the basis of balance of that friend

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -17,10 +17,52 @@ const initialFriends = [
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
   },
-];
+]; // Array of friends information to be displayed
 
 function App() {
-  return <div className="App">Shantanu</div>;
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
+    </div>
+  );
+} // Main component
+
+function FriendsList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  ); // Map through the friends array and return a Friend component for each friend
+}
+
+function Friend({ friend, key }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}₹
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {friend.balance}₹
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <button className="button">Select</button>
+    </li>
+  );
+  // Display the friend information in a list item and add a button to select the friend and display the transactions
+  //abs is used to convert negative balance to positive
 }
 
 export default App;
